5a0d764ca1d28dc488a1e2069bee373be214f172
marijaPesha <marija.pesha@gmail.com>
2021-02-02 23:01:01 +0100

    fetch orders

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,27 +4,10 @@ import { connect } from "react-redux";
 
 import CheckoutSummery from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData"
+import * as actions from "../../store/actions/index"
 
 
 class Checkout extends Component {
-    // state = {
-    //     ingredients: null,
-    //     price: 0
-    // }
-
-    // UNSAFE_componentWillMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     let price = 0;
-    //     for (let param of query.entries()) {
-    //         if (param[0] === "price") {
-    //             price = param[1]
-    //         } else {
-    //             ingredients[param[0]] = + param[1]
-    //         }
-    //     }
-    //     this.setState({ ingredients: ingredients, totalPrice: price })
-    // }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack()
@@ -37,9 +20,12 @@ class Checkout extends Component {
 
     render() {
         let summary = <Redirect to="/" />
+
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummery
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -58,7 +44,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
 
